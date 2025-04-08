dcdd5352191da476f49e8b35ab8157131efe9f03
DishantSharma6106 <dishant.sharma.mat24@itbhu.ac.in>
2025-04-08 22:54:07 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
 from flask import Flask, request, jsonify import requests from bs4 import BeautifulSoup
 
 app = Flask(name)
